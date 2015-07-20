e55c1f3554fbb2fc7b2f04fdb41ac1af0a11f368
elearningplugins <elearningplugins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-20 20:59:51 +0000

    initial 1.0
    git-svn-id: https://plugins.svn.wordpress.org/insert-or-embed-camtasia-content-into-wp/tags/1.0@1202621 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ camtasia.php
@@ -0,0 +1,125 @@
+<?php
+/*
+Plugin Name: Insert or Embed Camtasia Content into Wordpress Trial
+Plugin URI: http://www.elearningplugins.com
+Description: Quickly embed or insert Camtasia content into a post or page
+Version: 1.0
+Author: elearningplugins.com
+Author URI: http://www.elearningplugins.com
+*/
+
+define ( 'WP_camtasia_EMBEDER_PLUGIN_DIR', dirname(__FILE__)); // Plugin Directory
+define ( 'WP_camtasia_EMBEDER_PLUGIN_URL', plugin_dir_url(__FILE__)); // Plugin URL (for http requests)
+
+global $wpdb;
+require_once("settings_file.php");
+require_once("functions.php");
+include_once(WP_camtasia_EMBEDER_PLUGIN_DIR."/include/shortcode.php");
+
+
+
+
+
+register_activation_hook(__FILE__,'camtasia_embeder_install'); 
+
+
+register_deactivation_hook( __FILE__, 'camtasia_embeder_remove' );
+
+function camtasia_embeder_install() {
+
+@mkdir(camtasia_getUploadsPath());
+@file_put_contents(camtasia_getUploadsPath()."index.html","");
+
+}
+function camtasia_embeder_remove() {
+
+$camtasia_upload_path=camtasia_getUploadsPath();
+if(file_exists($camtasia_upload_path."/.htaccess")){unlink($camtasia_upload_path."/.htaccess");}
+
+}
+
+
+add_action( 'wp_ajax_camtasia_upload', 'wp_ajax_camtasia_upload' );
+add_action( 'wp_ajax_camtasia_del_dir', 'wp_ajax_camtasia_del_dir' );
+add_action( 'wp_ajax_camtasia_rename_dir', 'wp_ajax_camtasia_rename_dir');
+
+
+function wp_camtasia_media_button() {
+	$wp_camtasia_media_button_image = camtasia_getPluginUrl().'camtasia.png';
+	echo '<a href="media-upload.php?type=camtasia_upload&TB_iframe=true&tab=upload" class="thickbox">
+  <img src="'.$wp_camtasia_media_button_image.'"  width=15 height=15 /></a>';
+}
+
+function media_upload_camtasia_form()
+{
+	camtasia_print_tabs();
+	echo '<div class="wrap" style="margin-left:20px;  margin-bottom:50px;">';
+		echo '<div id="icon-upload" class="icon32"><br></div><h2>Upload File</h2>';
+		camtasia_print_upload();
+	echo "</div>";
+
+}
+function media_upload_camtasia_content()
+{
+	camtasia_print_tabs();
+	echo '<div class="wrap" style="margin-left:20px;  margin-bottom:50px;">';
+		echo '<div id="icon-upload" class="icon32"><br></div><h2>Camtasia Content Library</h2>';
+		camtasia_printInsertForm();
+	echo "</div>";
+}
+
+
+
+function media_upload_camtasia()
+{
+	wp_iframe( "media_upload_camtasia_content" );
+}
+
+function media_upload_camtasia_upload()
+{
+	if ( isset( $_REQUEST[ 'tab' ] ) && strstr( $_REQUEST[ 'tab' ], 'camtasia') )
+	{
+	wp_iframe( "media_upload_camtasia_content" );
+	}
+	else
+	{
+	wp_iframe( "media_upload_camtasia_form" );
+	}
+}
+
+function camtasia_print_tabs()
+{
+
+	
+	function camtasia_tabs($tabs) 
+	{
+	$newtab1 = array('upload' => 'Upload File');
+	$newtab2 = array('camtasia' => 'Content Library');
+	return array_merge($newtab1,$newtab2);
+	}
+add_filter('media_upload_tabs', 'camtasia_tabs');
+media_upload_header();
+
+}
+
+
+add_action('media_upload_camtasia_upload','media_upload_camtasia_upload');
+add_action('media_upload_camtasia','media_upload_camtasia');
+add_action( 'media_buttons', 'wp_camtasia_media_button',100);
+
+
+/* added by oneTarek --*/
+//add_action('wp_head','camtasia_embeder_wp_head');
+add_action('wp_footer','camtasia_embeder_wp_head');
+
+function camtasia_embeder_enqueue_script() {
+	wp_enqueue_script('jquery');
+}    
+ 
+add_action('wp_enqueue_scripts', 'camtasia_embeder_enqueue_script');
+
+include_once(WP_camtasia_EMBEDER_PLUGIN_DIR."/admin_page.php");
+
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,68 @@
+=== Insert or Embed Camtasia Content into Wordpress Trial ====
+Contributors: elearningplugins.com
+Donate link: http://www.elearningplugins.com
+Tags: Camtasia, insert, embed, iframe, elearning, mp4, techsmith, articulate, replay, premiere
+Requires at least: 4.2.2
+Tested up to: 4.2.2
+Stable tag: 1.0
+Quickly embed or insert Camtasia content into a post or page
+
+== Description ==
+
+This plugin will add a new toolbar icon (the Camtasia logo) next to the 'Add Media' button on the Edit Post and Edit Page pages.  Upon clicking this toolbar icon, you will have the ability to upload your published Camtasia content as a ZIP file.  Once uploaded, the plugin will automatically extract the content, find the appropriate .html file, and add code to your post or page that will display your Camtasia content as an iframe or a lightbox.
+
+Built for Camtasia, but works with any MP4 file!  Try it with MP4 files created with Articulate Replay, Adobe Premiere, or any other video production software!
+
+== Installation ==
+
+1. Upload the 'insert-or-embed-Camtasia-into-wordpress' folder to the `/wp-content/plugins/` directory. Activate the plugin through the 'Plugins' menu in WordPress
+
+== How to Use ==
+
+1. Create a new post or page
+2. Click on the Camtasia logo
+3. Browse for a MP4 file or .zip file that you created using Camtasia
+4. Upload and go!
+
+https://www.youtube.com/watch?v=fSEHeIatOzE
+
+
+== Frequently Asked Questions ==
+
+= How do I use this to embed Camtasia content? =
+
+1. Create a new post or page
+2. Click on the Camtasia logo
+3. Browse for a MP4 file or .zip file that you created using Camtasia
+4. Upload and go!
+
+= Why does the upload never finish or I get a -1 error message? =
+
+In order to resolve this issue, you need to update your php.ini in your wp-admin folder to reflect the following: 
+
+post_max_size = 50M
+
+max_execution_time = 60
+
+max_input_time = 60
+
+upload_max_filesize = 50M
+
+
+(These settings will vary depending upon your server and content.  You may need to contact your hosting company to make these changes.) 
+
+For additional troubleshooting information, see:  http://www.elearningplugins.com/increase-maximum-upload-file-size/
+
+= How do I increase the maximum file size? =
+
+http://www.elearningplugins.com/increase-maximum-upload-file-size/
+
+= If I delete the plugin, what happens to the content that I've uploaded? =
+
+The uploaded content is saved into the wp-content / uploads / Camtasia_uploads folder on your site.  Thus, your uploaded content will not be removed if you delete this plugin.
+
+== Changelog ==
+
+= 1.0 =
+
+Initial version.
